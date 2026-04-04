9ab5852d3823eb95e41256ae863888fe40b16856
kvho <kvho@umich.edu>
2026-04-04 20:13:41 +0200

    update tempheading in info.js and set base path in Vite config

--- src/data/info.js
+++ src/data/info.js
@@ -1,6 +1,6 @@
 export const info = {
   name: "Khanh Vy (Vivian) O",
-  tempheading: "Vivian O (Click here for more info!)",
+  tempheading: "Vivian O | Computer Science + Cognitive Science @ University of Michigan",
   shortName: "Vivian O",
   tagline: "CS + Cognitive Science @ University of Michigan",
   bio: "I'm an undergraduate double-majoring in Computer Science and Cognitive Science at the University of Michigan–Ann Arbor. My work centers on designing inclusive technical solutions with meaningful social impact, particularly across civic engagement, education, and healthcare.",
