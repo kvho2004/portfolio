ff4d8af0feac220cb77a78f63c65f08ede07b1a9
kvho <kvho@umich.edu>
2026-07-16 16:19:30 -0400

    Updated bio

--- src/data/info.js
+++ src/data/info.js
@@ -4,7 +4,7 @@ export const info = {
     "Vivian O | Computer Science + Cognitive Science @ University of Michigan",
   shortName: "Vivian O",
   tagline: "CS + Cognitive Science @ University of Michigan",
-  bio: "I build full-stack, AI-powered applications at the intersection of technology and human cognition, with a focus on education, accessibility, and real-world impact. ",
+  bio: "Driven by a passion for human-centered technology, with active interests in cybersecurity, accessibility, and edtech. ",
   email: "kvho@umich.edu",
   github: "https://github.com/kvho2004",
   linkedin: "https://www.linkedin.com/in/kvho/",
